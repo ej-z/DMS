9b4738b1f6a71416b95de0131fa578114e6bfba8
ej-z <jazzmeister.725@gmail.com>
2017-01-03 17:09:54 +0530

    Changed .Net version to 4.0 client profile. Required for DataGrid which is being used by repeaters. Removed the previous method of ListView + Dynamic Grid

--- DMS/Controls/RepeaterControl.xaml.cs
+++ DMS/Controls/RepeaterControl.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Forms;
+using System.Windows.Data;
+using DocumentManipulation;
 using UserControl = System.Windows.Controls.UserControl;
+using System.Windows.Controls;
 
 namespace DMS
 {
@@ -12,8 +14,11 @@ namespace DMS
     /// </summary>
     public partial class RepeaterControl : UserControl
     {
-        int row = 1;
         DocumentManipulation.Repeater repeater;
+
+        private ObservableCollection<Record> _records;
+
+        ObservableCollection<Record> Records { get { return _records; } set { _records = value; } }
 
         public RepeaterControl(string label, DocumentManipulation.Repeater repeater)
         {
@@ -23,13 +28,14 @@ namespace DMS
             this.SetValue(Grid.RowProperty, repeater.Row);
             this.SetValue(Grid.ColumnProperty, repeater.Column);
             this.SetValue(Grid.ColumnSpanProperty, repeater.ColumnSpan);
-            int i = 0;
-            foreach (var key in repeater.GridHeaders())
+            this.DataContext = this;
+            _records = new ObservableCollection<Record>();
+            dataGrid.ItemsSource = Records;
+
+            foreach (var header in repeater.GridHeaders())
             {
-                HeaderGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
-                var header = ControlFactory.GenerateRepeaterHeaderControl(key);
-                header.SetValue(Grid.ColumnProperty, i++);
-                HeaderGrid.Children.Add(header);
+                var binding = new Binding($"Attributes[{header.Index}]");
+                dataGrid.Columns.Add(new CustomBoundColumn() { Header = header.Label, Binding = binding, TemplateName = "CustomTemplate" });
             }
             SetCountLabel();
         }       
@@ -39,68 +45,88 @@ namespace DMS
             CountLabel.Content = repeater.CountLabel + " " + repeater.Count;
         }
 
-        private void Add_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void Add_Click(object sender, RoutedEventArgs e)
         {
             var i = repeater.CloneInput();
             InputWindow iw = new InputWindow(repeater.RepeaterData[i]);
             iw.ShowDialog();
-            AddRow(repeater.RowValues(i));
+            _records.Add(new Record(repeater.RowValues(i)));
             SetCountLabel();
         }
 
-        private void AddRow(IEnumerable<string> values)
+        private void Edit_Click(object sender, RoutedEventArgs e)
         {
-            Grid row = new Grid();
-            int i = 0;
-            foreach (var value in values)
+            if (dataGrid.SelectedItems.Count == 0)
             {
-                row.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
-                var cell = ControlFactory.GenerateRepeaterRowControl(value);
-                cell.SetValue(Grid.ColumnProperty, i++);
-                row.Children.Add(cell);
-            }
-
-            var item = new System.Windows.Controls.ListViewItem();
-            item.Content = row;
-            RepeaterHolder.Items.Add(item);
-        }
-
-        private void Edit_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            if (RepeaterHolder.SelectedItems.Count == 0)
-            {
-                System.Windows.MessageBox.Show("Please select a row.");
+                MessageBox.Show("Please select a row.");
                 return;
             }
-            var i = RepeaterHolder.Items.IndexOf(RepeaterHolder.SelectedItems[0]);
+            var i = dataGrid.Items.IndexOf(dataGrid.SelectedItems[0]);
             InputWindow iw = new InputWindow(repeater.RepeaterData[i]);
             iw.ShowDialog();
-            EditRow(repeater.RowValues(i), (Grid)((System.Windows.Controls.ListViewItem)RepeaterHolder.SelectedItems[0]).Content);
+            dataGrid.ItemsSource = null;
+            dataGrid.ItemsSource = Records;
         }
 
-        private void EditRow(IEnumerable<string> values, Grid row)
+        private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            int i = 0;
-            row.Children.Clear();
-            foreach (var value in values)
+            if (dataGrid.SelectedItems.Count == 0)
             {
-                var cell = ControlFactory.GenerateRepeaterRowControl(value);
-                cell.SetValue(Grid.ColumnProperty, i++);
-                row.Children.Add(cell);
-            }
-        }
-
-        private void Delete_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            if (RepeaterHolder.SelectedItems.Count == 0)
-            {
-                System.Windows.MessageBox.Show("Please select a row.");
+                MessageBox.Show("Please select a row.");
                 return;
             }
-            var i = RepeaterHolder.Items.IndexOf(RepeaterHolder.SelectedItems[0]);
-            RepeaterHolder.Items.RemoveAt(i);
+            var i = dataGrid.Items.IndexOf(dataGrid.SelectedItems[0]);
+            _records.RemoveAt(i);
             repeater.RemoveAt(i);
             SetCountLabel();
+        }
+    }
+
+    public class CustomBoundColumn : DataGridBoundColumn
+    {
+        public string TemplateName { get; set; }
+
+        public CustomBoundColumn()
+        {
+            Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            var binding = new Binding(((Binding)Binding).Path.Path);
+            binding.Source = dataItem;
+
+            var content = new ContentControl();
+            content.ContentTemplate = (DataTemplate)cell.FindResource(TemplateName);
+            content.SetBinding(ContentControl.ContentProperty, binding);
+            return content;
+        }
+
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            return GenerateElement(cell, dataItem);
+        }
+    }
+    public class Record
+    {
+        private readonly ObservableCollection<Attribute> attributes = new ObservableCollection<Attribute>();
+
+        public Record(IEnumerable<Attribute> attributes)
+        {
+            foreach (var attribute in attributes)
+                Attributes.Add(attribute);
+        }
+
+        public void Edit(IEnumerable<Attribute> newAttributes)
+        {
+            var newAttr = newAttributes.ToArray();
+            for (int i = 0; i < newAttr.Count(); i++)
+                this.attributes[i] = newAttr[i];
+        }
+
+        public ObservableCollection<Attribute> Attributes
+        {
+            get { return attributes; }
         }
     }
 }
